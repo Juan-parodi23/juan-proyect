8c0dde5e4d4b5bd6ecdf1227fc59becf27340656
Juan-parodi23 <ninoparodi2007@gmail.com>
2024-12-10 03:07:26 +0100

    Add files via upload

---
+++ javascript.js/javascript.js
@@ -0,0 +1,10 @@
+
+function cambiarfondo1()
+{document.body.style.backgroundImage = "none";
+   document.body.style.backgroundColor = "black";
+document.body.style.color = "white"
+}
+alert("En esta pagina encontraras lo que necesites para estar a la moda")
+function cambiarfondo2()
+{alert("Pagina creada y diseñada por el autor: Juan Parodi")
+  }
